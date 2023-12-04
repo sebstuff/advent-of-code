13683e688b4c5c90f23f6ba82d48f38b6691d4da
Steven B <steven@botzek.com>
2023-12-04 12:47:10 -0800

    Solve 2023, day 4, part 2

--- 2023/day04.clj
+++ 2023/day04.clj
@@ -11,18 +11,43 @@
      :winning-numbers winning-numbers
      :picks picks}))
 
-(defn score-card
+(defn card-matches
   [{:keys [winning-numbers picks]}]
-  (let [matches (count (clojure.set/intersection (set winning-numbers) (set picks)))]
-    (long (Math/pow 2 (dec matches)))))
+  (count (clojure.set/intersection (set winning-numbers) (set picks))))
+
+(defn card-score
+  [card]
+    (long (Math/pow 2 (dec (card-matches card)))))
 
 (defn part1
   [filename]
   (->> (slurp filename)
        (str/split-lines)
        (mapv parse-card)
-       (mapv score-card)
+       (mapv card-score)
        (apply +)))
 
 (assert (= 13 (part1 "day04.example")))
-(assert (= 18619 (part1 "day04.input")))
+(time (assert (= 18619 (part1 "day04.input"))))
+
+(defn part2
+  [filename]
+  (let [cards (->> (slurp filename)
+                   (str/split-lines)
+                   (mapv parse-card))]
+    (loop [[card-count & card-counts] (repeat (count cards) 1)
+           [card & cards] cards
+           total-cards 0]
+      (if-not card
+        total-cards
+        (let [matches (card-matches card)
+              card-counts' (reduce (fn [acc n]
+                                     (if (>= n (count acc))
+                                       acc
+                                       (update acc n #(+ % card-count))))
+                                   (vec card-counts)
+                                   (range 0 matches))]
+          (recur card-counts' cards (+ total-cards card-count)))))))
+
+(assert (= 30 (part2 "day04.example")))
+(time (assert (= 8063216 (part2 "day04.input"))))
