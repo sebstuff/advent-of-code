569f8005b711e73f9b70f11056e1570f53627e67
Steven B <steven@botzek.com>
2023-12-07 15:26:48 -0800

    Refactor to use dynamic

--- 2023/day07.clj
+++ 2023/day07.clj
@@ -2,97 +2,59 @@
   (:require [clojure.string :as str]
             [clojure.pprint :refer [pprint]]))
 
+(def ^:dynamic *hand-strength* nil)
+(def ^:dynamic *suit-strengths* nil)
+
 (defn hand-strength
-  [hand wild?]
-  (if wild?
-    (let [card-matches (->> (frequencies hand)
-                            (map reverse)
-                            (sort-by first >))
-          wildcards (first (filter #(= \J (second %)) card-matches))
-          card-matches (filterv #(not= \J (second %)) card-matches)
-          card-matches (if (nil? wildcards)
-                         card-matches
-                         (update card-matches 0 (fn apply-wildcards[[count card]]
-                                                  (if (nil? count)
-                                                    wildcards
-                                                    [(+ count (first wildcards)) card]))))
-          card-matches (map first card-matches)]
-      (cond
-        ;; 5 of a kind
-        (= [5] card-matches)
-        7
+  [hand]
+  (let [card-matches (->> (frequencies hand)
+                          (vals)
+                          (sort >))]
+    (cond
+      ;; 5 of a kind
+      (= [5] card-matches)
+      7
 
-        ;; 4 of a kind
-        (= [4 1] card-matches)
-        6
+      ;; 4 of a kind
+      (= [4 1] card-matches)
+      6
 
-        ;; full house
-        (= [3 2] card-matches)
-        5
+      ;; full house
+      (= [3 2] card-matches)
+      5
 
-        ;; 3 of a kind
-        (= [3 1 1] card-matches)
-        4
+      ;; 3 of a kind
+      (= [3 1 1] card-matches)
+      4
 
-        ;; 2 pair
-        (= [2 2 1] card-matches)
-        3
+      ;; 2 pair
+      (= [2 2 1] card-matches)
+      3
 
-        ;; 1 pair
-        (= 2 (first card-matches))
-        2
+      ;; 1 pair
+      (= 2 (first card-matches))
+      2
 
-        ;; high card
-        :else
-        1))
-    (let [card-matches (->> (frequencies hand)
-                            (vals)
-                            (sort >))]
-      (cond
-        ;; 5 of a kind
-        (= [5] card-matches)
-        7
-
-        ;; 4 of a kind
-        (= [4 1] card-matches)
-        6
-
-        ;; full house
-        (= [3 2] card-matches)
-        5
-
-        ;; 3 of a kind
-        (= [3 1 1] card-matches)
-        4
-
-        ;; 2 pair
-        (= [2 2 1] card-matches)
-        3
-
-        ;; 1 pair
-        (= 2 (first card-matches))
-        2
-
-        ;; high card
-        :else
-        1))))
-(assert (= (hand-strength "AAAAA" false) 7))
-(assert (= (hand-strength "KAAAA" false) 6))
-(assert (= (hand-strength "AAKAA" false) 6))
-(assert (= (hand-strength "AAAAK" false) 6))
-(assert (= (hand-strength "AAAAK" false) 6))
-(assert (= (hand-strength "AAAKK" false) 5))
-(assert (= (hand-strength "AKAKA" false) 5))
-(assert (= (hand-strength "KAAAK" false) 5))
-(assert (= (hand-strength "AKAJA" false) 4))
-(assert (= (hand-strength "AKATK" false) 3))
-(assert (= (hand-strength "A234A" false) 2))
-(assert (= (hand-strength "QKJTA" false) 1))
+      ;; high card
+      :else
+      1)))
+(assert (= (hand-strength "AAAAA") 7))
+(assert (= (hand-strength "KAAAA") 6))
+(assert (= (hand-strength "AAKAA") 6))
+(assert (= (hand-strength "AAAAK") 6))
+(assert (= (hand-strength "AAAAK") 6))
+(assert (= (hand-strength "AAAKK") 5))
+(assert (= (hand-strength "AKAKA") 5))
+(assert (= (hand-strength "KAAAK") 5))
+(assert (= (hand-strength "AKAJA") 4))
+(assert (= (hand-strength "AKATK") 3))
+(assert (= (hand-strength "A234A") 2))
+(assert (= (hand-strength "QKJTA") 1))
 
 (defn ->player
-  [hand bid wild?]
+  [hand bid]
   {:hand hand
-   :strength (hand-strength hand wild?)
+   :strength (*hand-strength* hand)
    :bet bid})
 
 (def suit-strengths
@@ -109,6 +71,59 @@
    \Q 11
    \K 12
    \A 13})
+
+(defn compare-player
+  [p1 p2]
+  (let [strength-compare (compare (:strength p1) (:strength p2))]
+    (if (zero? strength-compare)
+      (loop [[card1 & hand1] (:hand p1)
+             [card2 & hand2] (:hand p2)]
+        (let [card-compare (compare (*suit-strengths* card1) (*suit-strengths* card2))]
+          (if (zero? card-compare)
+            (recur hand1 hand2)
+            card-compare)))
+      strength-compare)))
+
+(defn parse-player
+  [s]
+  (let [[_ hand bid] (re-find #"^([2-9TJQKA]{5,5}) +(\d+)$" s)]
+    (->player hand (parse-long bid))))
+
+(defn parse-players
+  [s]
+  (->> s
+       (str/split-lines)
+       (mapv parse-player)))
+
+(defn part1
+  [filename]
+  (binding [*hand-strength* hand-strength
+            *suit-strengths* suit-strengths]
+    (->> (slurp filename)
+         (parse-players)
+         (sort compare-player)
+         (map-indexed (fn [idx player] (* (inc idx) (:bet player))))
+         (apply +))))
+
+(assert (= 6440 (part1 "day07.example")))
+(assert (= 250602641 (time (part1 "day07.input"))))
+
+(defn wild-hand-strength
+  [hand]
+  (let [card-matches (->> (frequencies hand)
+                          (map reverse)
+                          (sort-by first >))
+        wildcards (first (filter #(= \J (second %)) card-matches))
+        card-matches (filterv #(not= \J (second %)) card-matches)
+        card-matches (if (nil? wildcards)
+                       card-matches
+                       (update card-matches 0 (fn apply-wildcards[[count card]]
+                                                (if (nil? count)
+                                                  wildcards
+                                                  [(+ count (first wildcards)) card]))))
+        new-hand (apply concat (map #(repeat (first %) (second %)) card-matches))]
+    (hand-strength new-hand)))
+
 (def wild-suit-strengths
   {\2 1
    \3 2
@@ -124,50 +139,15 @@
    \K 12
    \A 13})
 
-(defn compare-player
-  [p1 p2 wild?]
-  (let [strength-compare (compare (:strength p1) (:strength p2))]
-    (if (zero? strength-compare)
-      (loop [[card1 & hand1] (:hand p1)
-             [card2 & hand2] (:hand p2)]
-        (let [card-compare (if wild?
-                             (compare (wild-suit-strengths card1) (wild-suit-strengths card2))
-                             (compare (suit-strengths card1) (suit-strengths card2)))]
-          (if (zero? card-compare)
-            (recur hand1 hand2)
-            card-compare)))
-      strength-compare)))
-
-(defn parse-player
-  [s wild?]
-  (let [[_ hand bid] (re-find #"^([2-9TJQKA]{5,5}) +(\d+)$" s)]
-    (->player hand (parse-long bid) wild?)))
-
-(defn parse-players
-  [wild? s]
-  (->> s
-       (str/split-lines)
-       (mapv #(parse-player % wild?))))
-
-(defn part1
-  [filename]
-  (->> (slurp filename)
-       (parse-players false)
-       (sort #(compare-player %1 %2 false))
-       (map-indexed (fn [idx player] (* (inc idx) (:bet player))))
-       (apply +)))
-
-(assert (= 6440 (part1 "day07.example")))
-(assert (= 250602641 (time (part1 "day07.input"))))
-
-
 (defn part2
   [filename]
-  (->> (slurp filename)
-       (parse-players true)
-       (sort #(compare-player %1 %2 true))
-       (map-indexed (fn [idx player] (* (inc idx) (:bet player))))
-       (apply +)))
+  (binding [*hand-strength* wild-hand-strength
+            *suit-strengths* wild-suit-strengths]
+    (->> (slurp filename)
+         (parse-players)
+         (sort compare-player)
+         (map-indexed (fn [idx player] (* (inc idx) (:bet player))))
+         (apply +))))
 
 (assert (= 5905 (part2 "day07.example")))
 (assert (= 251037509 (time (part2 "day07.input"))))
