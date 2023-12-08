2da5dd1e0904ea87e3509db16b170dba3a5dbd66
Steven B <steven@botzek.com>
2023-12-07 21:38:41 -0800

    Solve 2023, day 8, part 2

--- 2023/day08.clj
+++ 2023/day08.clj
@@ -1,6 +1,7 @@
 (ns day08
   (:require [clojure.string :as str]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [clojure.math.numeric-tower :refer [lcm]]))
 
 (def start-node "AAA")
 (def end-node "ZZZ")
@@ -8,7 +9,7 @@
 (defn parse-input
   [s]
   (let [[_ instructions] (re-find #"^([RL]+)\n\n" s)
-        nodes (->> (re-seq #"([A-Z]{3,3}) = \(([A-Z]{3,3}), ([A-Z]{3,3})\)" s)
+        nodes (->> (re-seq #"([A-Z0-9]{3,3}) = \(([A-Z0-9]{3,3}), ([A-Z0-9]{3,3})\)" s)
                    (map (fn [[_ node left right]]
                           {:node node
                            :left left
@@ -41,3 +42,31 @@
 
 (assert (= 2 (time (part1 "day08.example"))))
 (assert (= 16271 (time (part1 "day08.input"))))
+
+
+(defn walk2
+  [{:keys [instructions nodes]} start-node]
+  (loop [[instruction & instructions'] instructions
+         on-node (get nodes start-node)
+         steps 1]
+    (let [next-node-name (if (= \R instruction)
+                           (:right on-node)
+                           (:left on-node))
+          next-node (get nodes next-node-name)]
+      (if (str/ends-with? next-node-name "Z")
+        steps
+        (let [instructions' (if (empty? instructions')
+                              instructions
+                              instructions')]
+          (recur instructions' next-node (inc steps)))))))
+
+;;; This works by finding the step count for each start node, then finding the least common multiple of all of them.
+(defn part2
+  [filename]
+  (let [{:keys [nodes] :as input} (parse-input (slurp filename))
+        start-nodes (filterv #(str/ends-with? % "A") (keys nodes))
+        start-node-steps (mapv #(walk2 input %) start-nodes)]
+    (reduce lcm 1 start-node-steps)))
+
+(assert (= 6 (time (part2 "day08.example2"))))
+(assert (= 14265111103729 (time (part2 "day08.input"))))
