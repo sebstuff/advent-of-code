5d18d5646f07111781880852ee08f18056aa5299
Steven B <steven@botzek.com>
2024-12-02 22:33:47 -0800

    Solve 2024, day 2, part 2

--- 2024/day02.clj
+++ 2024/day02.clj
@@ -9,9 +9,9 @@
 
 (defn level-safe?
   [level]
-  (let [partitioned (partition 2 1 level)]
-    (or (every? (fn [[l r]] (<= 1 (- r l) 3 )) partitioned)
-        (every? (fn [[l r]] (<= 1 (- l r) 3 )) partitioned))))
+  (let [pairs (partition 2 1 level)]
+    (or (every? (fn [[l r]] (<= 1 (- r l) 3 )) pairs)
+        (every? (fn [[l r]] (<= 1 (- l r) 3 )) pairs))))
 
 (defn part1
   [filename]
@@ -22,3 +22,41 @@
        (count)))
 
 (println (part1 "day02.input"))
+
+(defn dampened-level-safe?
+  [level]
+  (let [pairs (partition 2 1 level)
+        safe-neg? (fn [[l r]] (<= 1 (- r l) 3 ))
+        safe-pos? (fn [[l r]] (<= 1 (- l r) 3 ))
+        unsafe-neg? (fn [pair] (not (safe-neg? pair)))
+        unsafe-pos? (fn [pair] (not (safe-pos? pair)))
+        unsafe-neg (count (filter unsafe-neg? pairs))
+        unsafe-pos (count (filter unsafe-pos? pairs))]
+    (cond
+      (= 0 unsafe-neg)
+      true
+
+      (= 0 unsafe-pos)
+      true
+
+      (and (or (<= 2 unsafe-neg) (<= 2 unsafe-pos))
+           (let [dampened-pairs (for [x (range (count level))
+                                      :let [level' (concat (take x level) (drop (inc x) level))]]
+                                  (partition 2 1 level'))]
+             (or (some #(every? safe-neg? %) dampened-pairs)
+                 (some #(every? safe-pos? %) dampened-pairs))))
+
+      true
+
+      :else
+      false)))
+
+(defn part2
+  [filename]
+  (->> (slurp filename)
+       (str/split-lines)
+       (map parse-level)
+       (filter dampened-level-safe?)
+       (count)))
+
+(println (part2 "day02.input"))
