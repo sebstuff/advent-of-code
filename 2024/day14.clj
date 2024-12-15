9ad60b822befc10ea61cd9b48ef2a26da66837fc
Steven B <steven@botzek.com>
2024-12-14 21:51:21 -0800

    Solve 2024, day 14, part 2

--- 2024/day14.clj
+++ 2024/day14.clj
@@ -75,3 +75,55 @@
 
 (assert (= 12 (part1 "day14.example" 100 [11 7])))
 (pp/pprint (time (part1 "day14.input" 100 [101 103])))
+
+(defn step
+  [robots dimensions]
+  (mapv (fn [robot]
+          (let [{:keys [position velocity]} robot
+                position' (mapv (fn [p v d]
+                                  (let [r (rem (+ p v)
+                                               d)]
+                                    (if (> 0 r)
+                                      (+ d r)
+                                      r)))
+                                position
+                                velocity
+                                dimensions)]
+            (assoc robot :position position')))
+        robots))
+
+(defn robot-str-map
+  [robots [dx dy :as dimensions]]
+  (apply str
+         (for [y (range dy)
+        x (range dx)]
+    (let [robots-at-pos (filterv #(= (:position %) [x y]) robots)
+          map-char (if (empty? robots-at-pos)
+                     "."
+                     "X")]
+      (if (= x (dec dx))
+        (str map-char "\n")
+        map-char)
+      ))))
+
+(defn part2
+  [filename dimensions]
+  (let [robots (->> (slurp filename)
+                    (parse-robots))
+        robots' (simulate robots 7051 dimensions)] ; Picture at 7051
+    (robot-str-map robots' dimensions)))
+
+(defn part2-search
+  [filename dimensions]
+  (let [robots (->> (slurp filename)
+                    (parse-robots))]
+    (loop [robots robots
+           seconds 0]
+      (let [s (robot-str-map robots dimensions)]
+        (when (or (= 0 (rem seconds 100))
+                  (str/index-of s "XXXXXXXXXXXXXXXXXXXXXXXX"))
+          (do (println "seconds" seconds)
+              (println s))))
+      (recur (step robots dimensions) (inc seconds)))))
+
+(println (part2 "day14.input" [101 103]))
