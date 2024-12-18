70d85f44dca2ebf2905147ca83bbb4fe0a9c30b4
Steven B <steven@botzek.com>
2024-12-18 08:44:18 -0800

    Solve 2024, day 18, part 2

--- 2024/day18.clj
+++ 2024/day18.clj
@@ -18,8 +18,14 @@
            costs {[0 0] 0}]
       (let [visiting (first (sort-by costs to-visit))
             cost (costs visiting)]
-        (if (= exit visiting)
+        (cond
+          (nil? visiting)
+          nil
+
+          (= exit visiting)
           cost
+
+          :else
           (let [neighbors (->> (neighbors visiting size)
                                (filter #(not (corrupted %)))
                                (filter #(not (costs %))))
@@ -41,3 +47,32 @@
 
 (assert (= 22 (part1 "day18.example" 7 12)))
 (println (time (part1 "day18.input" 71 1024)))
+
+
+(defn part2
+  [filename size]
+  (let [corrupted (->> (slurp filename)
+                       (str/split-lines)
+                       (mapv #(str/split % #","))
+                       (mapv (fn [[x y]] [(parse-long x) (parse-long y)])))]
+    (loop [lo-corrupted 0
+           hi-corrupted (count corrupted)]
+      (let [test-corrupted (quot (+ lo-corrupted hi-corrupted) 2)
+            cost-test (find-cost size test-corrupted corrupted)
+            cost-next-test (find-cost size (inc test-corrupted) corrupted)]
+        (cond
+          ;; both nil, we're too high
+          (and (nil? cost-test)
+               (nil? cost-next-test))
+          (recur lo-corrupted test-corrupted)
+
+          ;; neither nil, we're too low
+          (and cost-test cost-next-test)
+          (recur test-corrupted hi-corrupted)
+
+          ;; just right
+          :else
+          (nth corrupted test-corrupted))))))
+
+(assert (= [6 1] (part2 "day18.example" 7)))
+(println (time (part2 "day18.input" 71)))
