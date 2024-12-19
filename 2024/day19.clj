6bd9ccfc41c86e4e56253e27cb57a6ae2c0ed1ff
Steven B <steven@botzek.com>
2024-12-19 11:17:10 -0800

    Solve 2024, day 19, part 2
    Memoize is the way.

--- 2024/day19.clj
+++ 2024/day19.clj
@@ -30,33 +30,31 @@
 (assert (= 6 (part1 "day19.example")))
 (println (time (part1 "day19.input"))) ; 228
 
-(defn ways-make
-  ([patterns design]
-   (println "at" design)
-   (ways-make patterns design 0))
-  ([patterns design ways-sofar]
-   (if (= "" design)
-     (inc ways-sofar)
-     (loop [[pattern & patterns'] patterns
-            ways ways-sofar]
-       (cond
-         (not pattern)
-         ways
 
-         (str/starts-with? design pattern)
-         (recur patterns'
-                (ways-make patterns (subs design (count pattern)) ways))
+(def ways-make
+  (memoize (fn [patterns design]
+             (if (= "" design)
+               1
+               (loop [[pattern & patterns'] patterns
+                      ways 0]
+                 (cond
+                   (not pattern)
+                   ways
 
-         :else
-         (recur patterns' ways))))))
+                   (str/starts-with? design pattern)
+                   (let [new-ways (ways-make patterns (subs design (count pattern)))]
+                     (recur patterns' (+ new-ways ways)))
+
+                   :else
+                   (recur patterns' ways)))))))
 
 (defn part2
   [filename]
   (let [[patterns-str whitespace & designs] (str/split-lines (slurp filename))
         patterns (str/split patterns-str #", ")]
     (->> designs
-         (pmap #(ways-make patterns %))
+         (map #(ways-make patterns %))
          (apply +))))
 
 (assert (= 16 (part2 "day19.example")))
-#_(println (time (part2 "day19.input"))) ; takes too long
+(println (time (part2 "day19.input"))) ; 584553405070389
